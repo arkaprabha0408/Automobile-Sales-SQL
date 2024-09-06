69bb08b615dffbfad724feb0a88bdb1ee89bf936
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:57:58 +0530

    Create dealer data.sql

---
+++ Data Entry/dealer data.sql
@@ -0,0 +1,5 @@
+insert into dealer values(123,1,'Biswas Automobiles');
+insert into dealer values(456,2,'Venkatesh Motors');
+insert into dealer values(789,3,'APB Automobiles');
+insert into dealer values(321,4,'Ghosh Motors');
+insert into dealer values(654,5,'AG Automobiles');
