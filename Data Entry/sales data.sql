aad0ac8ce7075fa51734b9a3b8128630b2079342
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:59:42 +0530

    Create sales data.sql

---
+++ Data Entry/sales data.sql
@@ -0,0 +1,5 @@
+insert into Sales values(101,1001,1,123,5000000,'19-NOV-23');
+insert into Sales values(102,1002,2,456,3500000,'16-NOV-23');
+insert into Sales values(103,1003,3,789,4000000,'20-OCT-23');
+insert into Sales values(104,1004,4,321,300000,'15-OCT-23');
+insert into Sales values(105,1005,5,654,2000000,'17-NOV-23');
