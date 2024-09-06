414714f8e83bd74e8bca87a31700f4d080041f37
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:57:29 +0530

    Create brands data.sql

---
+++ Data Entry/brands data.sql
@@ -0,0 +1,5 @@
+insert into brand values(5001,'Audi',1);
+insert into brand values(5002,'Lamborgini',2);
+insert into brand values(5003,'Bentley',3);
+insert into brand values(5004,'Bugatti',4);
+insert into brand values(5005,'Skoda',5);
