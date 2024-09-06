3c24ba8652378876e45fed844ad1967eabd14577
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:58:53 +0530

    Create supplier data.sql

---
+++ Data Entry/supplier data.sql
@@ -0,0 +1,5 @@
+insert into Supplier values(555,'A4','Bhuiya Motors');
+insert into Supplier values(666,'Huracan','Gargy Automobiles');
+insert into Supplier values(777,'Flying Spur','Krishna Motors');
+insert into Supplier values(888,'Royale','Lokenath Motors');
+insert into Supplier values(999,'Slavia','Ayan Automobiles');
