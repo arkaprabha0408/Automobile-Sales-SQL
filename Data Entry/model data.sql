397c23190cb1eb4ec629be2255486f3109be03b0
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:58:14 +0530

    Create model data.sql

---
+++ Data Entry/model data.sql
@@ -0,0 +1,5 @@
+insert into Model values('A4',1,5001,123,'convertible','White','petrol engine');
+insert into Model values('Huracan',2,5002,456,'non-convertible','Yellow','V-12 engine');
+insert into Model values('Flying Spur',3,5003,789,'convertible','Red','twin-turbocharged engine');
+insert into Model values('Royale',4,5004,321,'non-convertible','Blue','W-16 engine');
+insert into Model values('Slavia',5,5005,654,'non-convertible','Orange','Four-cylinder engine');
