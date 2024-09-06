51967b40775f1d72eeb0b0a6658d7934ed12d95e
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:56:10 +0530

    Create Customer data.sql

---
+++ Data Entry/Customer data.sql
@@ -0,0 +1,5 @@
+insert into Customer values(1,'A','Kolkata','04-AUG-02',21,'Business','Male',6000000);
+insert into Customer values(1,'B','Delhi','21-JUL-73',50,'Job','Female',5000000);
+insert into Customer values(1,'C','Mumbai','04-SEP-62',61,'Job','Male',7000000);
+insert into Customer values(1,'D','Bangalore','15-AUG-69',54,'Business','Male',3000000);
+insert into Customer values(1,'E','Chennai','29-JUL-78',45,'Business','Female',2500000);
