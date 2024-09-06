4fe735778490e7586f6c5f8bd3af3a6881c0135a
Arka Prabha Biswas <111248437+arkaprabha0408@users.noreply.github.com>
2024-09-07 01:59:16 +0530

    Create vehicle data.sql

---
+++ Data Entry/vehicle data.sql
@@ -0,0 +1,5 @@
+insert into Vehicle values(1001,1,'A4',555,'Petrol');
+insert into Vehicle values(1002,2,'Huracan',666,'Petrol');
+insert into Vehicle values(1003,3,'Flying Spur',777,'diesel');
+insert into Vehicle values(1004,4,'Royale',888,'Petrol');
+insert into Vehicle values(1005,5,'Slavia',999,'diesel');
